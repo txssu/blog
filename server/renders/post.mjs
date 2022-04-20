ac57e00fec3143350e58f48f389ffa3b9a0ce1c3
Evgeniy Rybin <z.ribin20@gmail.com>
2022-04-20 20:14:11 +0500

    Render fields as ids if objects are not loaded

--- server/renders/post.mjs
+++ server/renders/post.mjs
@@ -5,13 +5,16 @@ export default function fullRender (post, content) {
   const data = {
     id: post.id,
     title: post.title,
-    author: userRender(post.Author),
-    tag: tagRender(post.Tag),
     photos: post.photos,
     createdAt: post.createdAt
   }
-  if (content) {
-    data.content = post.content
-  }
+  if (post.Author) data.author = userRender(post.Author)
+  else data.authorId = post.authorId
+
+  if (post.Tag) data.tag = tagRender(post.Tag)
+  else data.tagId = post.tagId
+
+  if (content) data.content = post.content
+
   return data
 }
